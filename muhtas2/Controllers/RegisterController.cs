1fae47b910dbb3072216311b01793a53baf9c7b2
karun <1harunkorkmaz@gmail.com>
2023-05-21 07:48:39 +0300

    add user edit contr.

--- muhtas2/Controllers/RegisterController.cs
+++ muhtas2/Controllers/RegisterController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entity.DbModel;
+using DataAccesss.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace muhtas2.Controllers
 {
+    [AllowAnonymous]
     public class RegisterController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IUserDal _userDal;
+
+        public RegisterController(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(User person)
+        {
+            int result = await _userDal.Add(person);
+            if (result == 0)
+                return View();
+            return BadRequest(result);
         }
     }
 }
